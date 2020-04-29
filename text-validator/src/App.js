650bc9e9abe19521192747c6d4abb4274ac1b9c7
JEffrey mcintyre <redeyecoding@gmail.com>
2020-04-29 11:21:43 -0400

    react: fixed bug for letter display, fixed bug for char deletion method

--- text-validator/src/App.js
+++ text-validator/src/App.js
@@ -1,26 +1,83 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ValidationComponent from './validation/ValidationComponent';
+import CharComponent from './validation/CharComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state ={
+      textLength: 0,
+      text: '',
+      listOfChars: [],
+    }
+  }
+
+  getTextHandler = event => {
+    let listOfChars;
+    let [textLength, text] = [
+            this.state.textLength, 
+            this.state.text
+          ];
+
+    text = event.target.value;
+    textLength = text.length;
+    listOfChars = text.split('')
+                      .map((char,index) => {
+                        return {id:index, char:char};
+                      });
+
+      this.setState({
+      textLength: textLength,
+      text: text,
+      listOfChars: listOfChars,
+    })
+  }
+
+  deleteCharHandler = ((event,index) => {   
+    const char = event.target.textContent
+    const listOfChars = [...this.state.listOfChars]
+    const updatedText = this.state.text.replace(char, '');
+
+    const updatedCharList = listOfChars.filter((charSet, set) => {
+      return charSet.id !== index
+    })
+
+    this.setState({
+      listOfChars: updatedCharList,
+      text: updatedText
+    })
+
+  });
+
+  render(){
+    const charBox = this.state.listOfChars
+                              .map((char, index) => {
+                                return (                          
+                                  <div className={'char-container'}>
+                                    <ul key={index} className={'char-styling'}>                                        
+                                          <CharComponent  deleteChar={(event) => this.deleteCharHandler(event, this.state.listOfChars[index].id)}
+                                                          singleChar={char.char}
+                                                           />                                        
+                                      </ul>
+                                  </div>)
+    });
+
+    return (
+      <div className="App">
+        <h1>Min Length Is 6 characters</h1>
+
+        <input type='text'
+                onChange={this.getTextHandler}
+                value={this.state.text} />
+
+        <ValidationComponent a
+            textLength={this.state.textLength}
+        />
+        {charBox}
+      </div>
+    )
+  }
 }
 
 export default App;
